2de7a5acb0e61250fc6d1dd84b1c307682fbe123
Daniel DaCosta <chaosape@gmail.com>
2012-04-14 23:15:23 -0500

    Change where the RMI registery get instantiated.

--- scripts/RunClient.sh
+++ scripts/RunClient.sh
@@ -3,4 +3,4 @@ PATH=$PATH:/home/kambala/software/jdk1.6.0_25/bin/
 rfile=`which $0`
 rfiledir=`dirname ${rfile}`
 . ${rfiledir}/support.sh
-(cd ${fqsrcdir} && java -cp .:${fqsrcdir}/:${fqjardir}commons-cli-1.2.jar Client $*)
+(cd ${fqsrcdir} &&  startRMIRegistry && java -cp .:${fqsrcdir}/:${fqjardir}commons-cli-1.2.jar Client $*)
--- scripts/RunComputeNode.sh
+++ scripts/RunComputeNode.sh
@@ -3,5 +3,4 @@ rfile=`which $0`
 PATH=$PATH:/home/kambala/software/jdk1.6.0_25/bin/
 rfiledir=`dirname ${rfile}`
 . ${rfiledir}/support.sh
-startRMIRegistry
-(cd ${fqsrcdir} && java -cp .:${fqsrcdir}/:${fqjardir}commons-cli-1.2.jar ComputeNode $*)
+(cd ${fqsrcdir} && startRMIRegistry && java -cp .:${fqsrcdir}/:${fqjardir}commons-cli-1.2.jar ComputeNode $*)
