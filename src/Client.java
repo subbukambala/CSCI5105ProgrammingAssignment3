e9d7c80fc9a8b4d2a7e403423d961e7d8114551b
Daniel DaCosta <chaosape@gmail.com>
2012-04-14 23:17:03 -0500

    Added to the client to begin to support list transfer. Made a comment change to ClientInterface.java

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,7 @@
 /**
- * @descriptrion Implements a Client read/write requests
- *  
+ * @descriptrion Implements a Client to query the psuedo-mapreduce framework
+ * for statistics and to submit a job.
+ *
  * @authors Bala Subrahmanyam Kambala, Daniel William DaCosta
  * @license GPLv3 (http://www.gnu.org/copyleft/gpl.html)
  */
@@ -14,95 +15,135 @@ import java.io.InputStreamReader;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.logging.Level;
-
+import java.util.List;
+import java.util.ArrayList;
 import org.apache.commons.cli.CommandLine;
 
 
 public class Client extends UnicastRemoteObject implements ClientInterface {
 
     private static Logger lg;
-    
+
     private static ServerInterface server;
-    
-    String id;
-    
+
     /**
      * The Client constructor.
      */
-    Client(String servername, String _id, boolean _debug) throws Exception {
-        id = _id;
-        // Handles proper read and write behavior.
-        if(!_debug) {
-            server = (ServerInterface) Naming.lookup("//" + servername + "/Server");
-            lg = new Logger("Client:" + id);
-        }
-        
+    Client(String servername ) throws Exception {
+        server = (ServerInterface) 
+            Naming.lookup("//" + servername + "/Server");
+        lg = new Logger("Client");
         lg.log(Level.FINER, "Client started.");
     }
-    
+
     @Override
-    public void jobResponse(TaskStats stats, String outputFilePath) throws RemoteException {
+        public void jobResponse(TaskStats stats, String outputFilePath) throws RemoteException {
         // prints job stats
     }
-    
+
     /**
-     * 
+     * Submit a job to the server.
+     * A job requires a filename.
+     * The filename should store a file containing 0 or more integers separated
+     * by newlines.
      */
-    private void submitJob() {
+    public  void submitJob(String filename) throws Exception {
+        lg.log(Level.FINEST, "File = " + filename);
+        FileInputStream fstream = new FileInputStream(filename);
+
+        // Get the object of DataInputStream
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String strLine;
         
+        List<Integer> data = new ArrayList<Integer>();
+        String strInt = null;
+        try {
+            // Read a list of integers from a file.
+            // The format of the file is very specific.
+            // Each integer will be separated by a new line.
+            while ((strInt = br.readLine()) != null) {
+                Integer i = new Integer(strInt);
+                data.add(i);
+            }
+        } catch(Exception e) {
+            System.out.println
+                (
+                 "File must consist of 0 or more integers separated "
+                 +"by newlines!"
+                 );
+            System.exit(1);
+        }
+            // Close the input stream
+        in.close();
     }
-    
+
     /**
-     * 
+     *
      */
     private void getNodeStats() {
-        
+
     }
-    
+
     public void transferData(
-            DataTransferInterface DTI,
-            Integer maxBufferWindow,
-            String srcFilePath,
-            String destFilePath) throws RemoteException 
+                             DataTransferInterface DTI,
+                             Integer maxBufferWindow,
+                             String srcFilePath,
+                             String destFilePath) throws RemoteException
     {
         DataTransferHandler d = new DataTransferHandler();
         d.transferData(DTI, maxBufferWindow, srcFilePath, destFilePath);
     }
 
-    public void storeData(String data, String filePath) throws RemoteException {
+    public void storeData(String data, String filePath) 
+        throws RemoteException {
         DataTransferHandler d = new DataTransferHandler();
         d.storeData(data, filePath);
     }
 
-    public void onDataTransferComplete(Integer jobId, String filePath) throws RemoteException {
+    public void onDataTransferComplete(Integer jobId, String filePath) 
+        throws RemoteException {
         // print sorted data here
         System.out.println("data transfer is complete");
     }
-    
+ 
     /**
-     * 
+     *
      */
     private void getServerStats() {
-        
+
     }
 
     public static void main(String[] argv) {
-
-        String id = null;
+        String server = "localhost";
+        boolean fileSwitch = false;
+        boolean nodeStatsSwitch = false;
+        boolean serverStatsSwitch = false;
         ArgumentHandler cli = new ArgumentHandler
-                (
-                        "Client [-h] [id] [Server address] [-n] [-s] "
-                        ,
-                        "A Client interface to Programming Assignment 3 for CSci5105."
-                                + " This client requests collector collector to"
-                                + " form a quorum which will ensure safe concurrent operations"
-                        ,
-                        "Bala Subrahmanyam Kambala, Daniel William DaCosta - GPLv3 (http://www.gnu.org/copyleft/gpl.html)"
-                );
+            (
+             "Client [-h|-n|-s] [Server address]  "
+             ,
+             "A Client interface to Programming Assignment 3 for CSci5105."
+             +" This client serves three primary objectives: 1) Submission"
+             +" of files to the pseudo-mapreduce framework for sorting,"
+             +" 2) Querying the server for compute node statistics,"
+             +" 3) Querying the server for server statistics."
+             ,"Bala Subrahmanyam Kambala, Daniel William DaCosta - GPLv3 "
+             +"(http://www.gnu.org/copyleft/gpl.html)"
+             );
 
         cli.addOption("h", "help", false, "Print this usage information.");
-        cli.addOption("n", "nodestats", true, "Prints the statistics of a node");
-        cli.addOption("s", "serverstats", false, "Prints sstatistics of a server");
+        cli.addOption("n","nodestats",true,"Prints the statistics of a node");
+        cli.addOption
+            ("s"
+             ,"serverstats"
+             ,false
+             ,"Prints statistics of a server");
+        cli.addOption
+            ("f"
+             ,"file"
+             ,true
+             ,"Submit a sort job to the server as a file.");
 
         // parse command line
         CommandLine commandLine = cli.parse(argv);
@@ -111,30 +152,52 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
             System.exit(0);
         }
 
-        if (commandLine.getArgs().length != 0)
-            id = commandLine.getArgs()[0];
-
-        if (id == null) {
-            cli.usage("Id required!");
+        if (commandLine.getArgs().length == 1)
+            server = commandLine.getArgs()[0];
+        else {
+            cli.usage("Client only accepts one argument!");
             System.exit(1);
         }
 
-        String server = "localhost";
-        if (commandLine.getArgs().length >= 2)
-            server = commandLine.getArgs()[1];
+        if (server == null) {
+            cli.usage("Server address required!");
+            System.exit(1);
+        }
 
         if (commandLine.hasOption('n')) {
-            System.out.print("Needs to be implemented.\n");
+            nodeStatsSwitch = true;
+            cli.usage("Unimplemented option!");
+            System.exit(1);
         }
 
         if (commandLine.hasOption('s')) {
-            System.out.print("Needs to be implemented.\n");
+            serverStatsSwitch = true;
+            cli.usage("Unimplemented option!");
+            System.exit(1);
         }
+
+        String filepath = null;
+        if (commandLine.hasOption('f')) {
+            fileSwitch = true;
+            filepath = commandLine.getOptionValue('f');
+            if (filepath == null) {
+                cli.usage("No file provided!\n");
+                System.exit(1);
+            }
+
+        }
+        
+        if ( serverStatsSwitch ^ nodeStatsSwitch ^ fileSwitch ) {
+                cli.usage("-n -s -f switches are mutually exclusive!\n");
+                System.exit(1);
+        }
+
 
         Client client = null;
         try {
-            client = new Client(server, id, false);
-            Naming.rebind("Client" + id, client);
+            client = new Client(server);
+            Naming.rebind("Client", client);
+            if ( fileSwitch ) client.submitJob(filepath);
         } catch (Exception e) {
             System.out.println("Client failed: ");
             e.printStackTrace();
--- src/ClientInterface.java
+++ src/ClientInterface.java
@@ -1,6 +1,7 @@
 /**
- * @descriptrion Remote Interface for a client. To simplify the logic, in order to 
- * communicate with client asynchronously, implementing RMI interface to client.
+ * @descriptrion Remote Interface for a client. To simplify the logic, in 
+ * order to communicate with client asynchronously, implementing RMI 
+ * interface to client.
  * 
  * @authors Bala Subrahmanyam Kambala, Daniel William DaCosta
  * @license GPLv3 (http://www.gnu.org/copyleft/gpl.html)
@@ -11,5 +12,6 @@ import java.rmi.RemoteException;
 
 public interface ClientInterface extends Remote, DataTransferInterface {
 
-    public void jobResponse(TaskStats stats, String outputFilePath) throws RemoteException;
+    public void jobResponse(TaskStats stats, String outputFilePath) 
+        throws RemoteException;
 }
